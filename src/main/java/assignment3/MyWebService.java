a42c72c7db5bb911e452bab28c53842676cd3269
avmorais <afonsovmorais@gmail.com>
2016-12-16 03:50:01 +0000

    ? still nothing

--- src/main/java/assignment3/MyWebService.java
+++ src/main/java/assignment3/MyWebService.java
@@ -3,11 +3,12 @@ package assignment3;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
-@WebService
+@WebService/*(endpointInterface = "src//main//java//assignment3.MyWebService",
+serviceName = "MyWebService")*/
 public class MyWebService {
 	
 	@WebMethod
 	public String subscribe(String email, String course){
-		return "Hello "+email+", you successfully subsribed to "+course+".";
+		return "Hello "+email+", you successfully subscribed to "+course+".";
 	}
 }
